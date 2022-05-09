966fbce6a278096472676850c388534749382ca9
deepikasampangi <deepikasampangi@gmail.com>
2022-05-09 10:14:02 +0530

    rename_main

--- app.py
+++ app.py
@@ -45,6 +45,6 @@ def complete(id):
         return 'There was an issue updating your task'
 
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host="0.0.0.0", port=port)
+# if __name__ == '__main__':
+#     port = int(os.environ.get('PORT', 8080))
+#     app.run(host="0.0.0.0", port=port)
