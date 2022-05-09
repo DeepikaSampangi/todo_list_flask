c255a2e54becdf339e85cc39d7f975ba6ceb9204
deepikasampangi <deepikasampangi@gmail.com>
2022-05-09 09:54:15 +0530

    to_do_flask

---
+++ main.py
@@ -0,0 +1,50 @@
+import os
+from flask import Flask, request, render_template, url_for, redirect
+from firebase_admin import credentials, firestore, initialize_app
+import base64
+import json
+
+
+# Initialize Flask App
+app = Flask(__name__)
+
+# Initialize Firestore DB
+cred = credentials.Certificate("src/key.json")
+default_app = initialize_app(cred)
+db = firestore.client()
+todo_ref = db.collection('todos')
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    if request.method =='POST':
+        try:
+            todo_ref.add({'task': request.form['task', 'complete': False]})
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+
+    else:
+        try:
+            todos = []
+            for doc in todo_ref.stream():
+                todo = doc.to_dict()
+                todo['id'] = doc.id
+                todos.append(todo)
+            print(todos)
+            return render_template('index.html', todos=todos)
+        except:
+            return 'There was an issue loading your todos'
+
+
+@app.route('/complete/<id>')
+def complete(id):
+    try:
+        todo_ref.document(id).update({'completed': True})
+        return redirect('/')
+    except:
+        return 'There was an issue updating your task'
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host="0.0.0.0", port=port)
