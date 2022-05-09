ac4c6af4293c81b39b8010001e4307e022fde353
deepikasampangi <deepikasampangi@gmail.com>
2022-05-09 12:15:15 +0530

    todo_app

--- app.py
+++ app.py
@@ -46,5 +46,5 @@ def complete(id):
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
